55f1d60cae765002b4aaf794613e7ba64752aac0
AudwitNafi <audwitnafi@iut-dhaka.edu>
2024-12-16 16:46:18 +0600

    docker

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///./history.db'
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:1234@localhost:3306/history'
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:1234@mysql_container:3306/history'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
